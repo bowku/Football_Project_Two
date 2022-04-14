581ea75a4fe4df4ff69e4263be3bedd1c0b1591d
Moh <mohamed@somehwere.com>
2022-04-14 12:06:20 +0100

    FPT-23 #comment created and completed FootballPlayerRepo

--- src/main/java/com/example/demo/repo/FootballPlayerRepo.java
+++ src/main/java/com/example/demo/repo/FootballPlayerRepo.java
@@ -1,5 +1,19 @@
 package com.example.demo.repo;
 
-public class FootballPlayerRepo {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.demo.domain.FootballPlayer;
+
+@Repository
+public interface FootballPlayerRepo extends JpaRepository<FootballPlayer, Integer> {
+
+	List<FootballPlayer> findByNameIgnoreCase(String name);
+
+	List<FootballPlayer> findByShirtNumber(Integer shirtNumber);
+
+	List<FootballPlayer> findByTeam(Integer team);
 
 }
